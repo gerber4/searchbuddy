1d8a26d72085cfbfdd3bf89fe938135320721c8f
Joseph Gerber <josephbgerber@gmail.com>
2022-01-29 15:18:48 -0500

    Made icon clearer and handled changing active tab.

--- extension/src/backgroundPage.ts
+++ extension/src/backgroundPage.ts
@@ -3,39 +3,60 @@ import MessageSender = Runtime.MessageSender;
 
 export type Msg = { type: "NewSearch"; content: string };
 
-let terms: string | null = null;
+let activeTab: number | null = null;
+let terms: Record<number, string | null> = {};
+
+function updateIcon() {
+    if (
+        activeTab != null &&
+        terms[activeTab] != null &&
+        terms[activeTab] != ""
+    ) {
+        browser.browserAction
+            .setIcon({ path: "exclaim-128.png" })
+            .catch((err) => {
+                console.log(err);
+            });
+    } else {
+        browser.browserAction
+            .setIcon({ path: "sleep-128.png" })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+}
+
+browser.tabs.onActivated.addListener((activeInfo) => {
+    activeTab = activeInfo.tabId;
+    updateIcon();
+});
 
 browser.runtime.onMessage.addListener((message: Msg, sender: MessageSender) => {
     switch (message.type) {
         case "NewSearch":
-            terms = message.content;
-
-            if (terms === null || terms === "") {
-                browser.browserAction
-                    .setIcon({ path: "sleep-128.png" })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-            } else {
-                browser.browserAction
-                    .setIcon({ path: "starry-128.png" })
-                    .catch((err) => {
-                        console.log(err);
-                    });
+            if (sender.tab?.id != undefined) {
+                terms[sender.tab.id] = message.content;
             }
+
+            updateIcon();
 
             console.log(message.content);
     }
 });
 
 browser.browserAction.onClicked.addListener(() => {
-    if (terms !== null && terms !== "") {
+    if (
+        activeTab != null &&
+        terms[activeTab] != null &&
+        terms[activeTab] != ""
+    ) {
         browser.tabs
             .create({
-                url: `searchbuddy.html?terms=${terms}`,
+                url: `searchbuddy.html?terms=${terms[activeTab]}`,
             })
             .catch((err) => {
                 console.log(err);
             });
+        updateIcon();
     }
 });
